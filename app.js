3db761e1751823108abc454f9ed44111af7c2c21
SamZaff <zaffieboy@yahoo.com>
2020-06-08 15:49:46 -0700

    printing dirname in app

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json());
 app.use((req, res, next) => {
-    //res.header("Access-Control-Allow-Origin", "*");
     console.log(req.path)
     next();
 })
+
+if (process.env.NODE_ENV === "production") {
+    console.log(__dirname)
+}
 
 app.use(express.static('./public'));
 app.get('/', (req, res) => res.send('Hello From Express!'))
