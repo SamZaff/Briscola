0cf27fc641aba5badf5ea7e8c31fa4bca2e00d0e
SamZaff <zaffieboy@yahoo.com>
2020-06-09 12:14:03 -0700

    Testing server logs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     next();
 })
 
-
+console.log('SERVER LOG TEST')
 
 app.use(express.static('./public'));
 app.get('/', (req, res) => res.send('Hello From Express!'))
