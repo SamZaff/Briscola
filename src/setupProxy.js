bbc68cc6019e1699ce5f1e82d39cf1a6687ddda1
SamZaff <zaffieboy@yahoo.com>
2020-06-09 23:35:54 -0700

    Testing server logs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    app.use(proxy({ target: 'http://localhost:4000' }));
-    // app.use(proxy('/otherApi/**', { target: 'http://localhost:5000' }));
+module.exports = function (app) {
+    app.use(
+        proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
+        );
 };
