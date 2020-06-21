f0b32c52dc68724cc254651617f480956a2cd6c7
SamZaff <zaffieboy@yahoo.com>
2020-06-20 21:35:10 -0700

    temp commenting out socketIO

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log('SERVER LOG TEST')
 const express = require('express');
 const app = express();
-const userCheck = require('./MongoDB/usercheck.js');
-const socketIO = require('./socketIO/server.js');
+const userCheck = require('/MongoDB/usercheck.js');
+// const socketIO = require('/socketIO/server.js');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 const http = require('http').Server(app); //POTENTIALLY REMOVE
@@ -31,52 +31,52 @@ app.use(function(req, res, next) {
     next();
 });
 
-io.on('connection', (socket) => {
-    if (socket.id) {
-        socket.on('getRooms', () => {
-            socketIO.sendRooms(io)
-        })
-        socket.on('cancel', (data) => {
-            socketIO.cancel(io, data)
-        })
-        socket.on('joinRoom', (data) => {
-            socketIO.joinRoom(socket, io, data)
-        })
-        socket.on('submitRoom', (data) => {
-            socketIO.submitRoom(socket, io, data)
-        })
-        socket.on('joinRequest', (data) => {
-            socketIO.joinRequest(socket, io, data)
-        })
-        socket.on('remove', (data) => {
-            socketIO.remove(socket, io, data)
-        })
-        socket.on('response', (data) => {
-            socketIO.response(io, data)
-        })
-        socket.on('drawCard', (data) => {
-            socketIO.drawCard(io, data)
-        })
-        socket.on('sendCard', (data) => {
-            socketIO.sendCard(io, data)
-        })
-        socket.on('clearField', (data) => {
-            socketIO.clearField(io, data)
-        })
-        socket.on('restartGame', (data) => {
-            socketIO.restartGame(io, data)
-        })
-        socket.on('message', (data) => {
-            socketIO.sendMessage(io, data)
-        })
-    }
-    socket.on('disconnect', () => {
-        console.log('someone has disconnected!');
-    });
-    socket.on('error', (e) => {
-        console.log(e);
-    });
-})
+// io.on('connection', (socket) => {
+//     if (socket.id) {
+//         socket.on('getRooms', () => {
+//             socketIO.sendRooms(io)
+//         })
+//         socket.on('cancel', (data) => {
+//             socketIO.cancel(io, data)
+//         })
+//         socket.on('joinRoom', (data) => {
+//             socketIO.joinRoom(socket, io, data)
+//         })
+//         socket.on('submitRoom', (data) => {
+//             socketIO.submitRoom(socket, io, data)
+//         })
+//         socket.on('joinRequest', (data) => {
+//             socketIO.joinRequest(socket, io, data)
+//         })
+//         socket.on('remove', (data) => {
+//             socketIO.remove(socket, io, data)
+//         })
+//         socket.on('response', (data) => {
+//             socketIO.response(io, data)
+//         })
+//         socket.on('drawCard', (data) => {
+//             socketIO.drawCard(io, data)
+//         })
+//         socket.on('sendCard', (data) => {
+//             socketIO.sendCard(io, data)
+//         })
+//         socket.on('clearField', (data) => {
+//             socketIO.clearField(io, data)
+//         })
+//         socket.on('restartGame', (data) => {
+//             socketIO.restartGame(io, data)
+//         })
+//         socket.on('message', (data) => {
+//             socketIO.sendMessage(io, data)
+//         })
+//     }
+//     socket.on('disconnect', () => {
+//         console.log('someone has disconnected!');
+//     });
+//     socket.on('error', (e) => {
+//         console.log(e);
+//     });
+// })
 
 console.log('DIRECTORY: ', path.resolve(__dirname))
 
